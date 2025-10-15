356637a57433ab666a3c79453bd28be543dd0db3
Adi432144 <74503284+Adi432144@users.noreply.github.com>
2025-10-15 23:58:51 +0530

    Update app_test.py

--- app_test.py
+++ app_test.py
@@ -282,8 +282,8 @@ if not st.session_state.username:
 else:
     st.markdown(f"<div id='greetingMessage'>Hello {st.session_state.username}, how are you feeling today?</div>", unsafe_allow_html=True)
     
-    mood_col2 = st.columns(4)
-    with mood_col2:
+    mood_cols = st.columns(4)
+    with mood_cols:
         st.markdown(f"<div id='moodDisplay'>Your current mood is: <strong>{st.session_state.mood}</strong></div>", unsafe_allow_html=True)
 
     mood_text = st.text_area("Type something about your mood...", placeholder=f"How are you feeling today, {st.session_state.username}?", height=100)
@@ -435,3 +435,4 @@ Experience stories that truly resonate with you — this app’s theme and narra
 </footer>
 
 """, unsafe_allow_html=True)
+
